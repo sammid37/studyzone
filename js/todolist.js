e342ffc1b3434a13d8acaf5ab96c1754234a9631
Felipe Tavares <felipe.oltavares@gmail.com>
2018-11-22 08:18:34 -0300

    List now accepts enter; Better styling;

--- js/todolist.js
+++ js/todolist.js
@@ -1,8 +1,23 @@
+function addListItem(item) {
+  $('.list').append('<div class="item">'+item+'</div>');
+}
+
+function addListItemFromInput() {
+  var input = $("input[name=todolistitem]");
+
+  addListItem(input.val());
+
+  input.val('');
+}
+
 $(document).ready(function() {
-  $("#tdl-button").click(function() { 
-      var toAdd = $("input[name=todolistitem]").val();
-      $('.list').append('<div class="item">'+toAdd+'</div>');
+  $("input[name=todolistitem]").on('keypress', function (e) {
+      if (e.which === 13) {
+          addListItemFromInput();
+      }
   });
+  $("#tdl-button").click(addListItemFromInput);
+
   $(document).on('click', '.item', function() {
       $(this).toggleClass("ok");
   });
